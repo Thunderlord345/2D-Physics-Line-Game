3066a3fc141ca34b257540f4c4892aab144a580a
Thunderlord345 <gregoryganis@gmail.com>
2022-12-28 02:41:21 +0800

    Antigrav Script

---
+++ Assets/Scripts/GravityZone.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityZone : MonoBehaviour
+{
+    public GameObject ball;
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = ball.GetComponent<Rigidbody2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        rb.gravityScale = -1;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        rb.gravityScale = 1;
+    }
+}
