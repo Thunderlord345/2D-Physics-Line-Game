7bd8066a91c251c76fe890291cd28b9a453e5d19
Thunderlord345 <gregoryganis@gmail.com>
2022-12-30 15:09:32 +0800

    Line Limit UI

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -12,17 +12,22 @@ public class ScoreManager : MonoBehaviour
     public int score = 0;
     public int highScore = 0;
 
+    public GameObject star1;
+    public GameObject star2;
+    public GameObject star3;
+
+    int levelNumber;
     private void Start()
     {
-        highScore = PlayerPrefs.GetInt("Highscore", 0);
-        scoreText.text = "Score: " + score.ToString();
+        highScore = PlayerPrefs.GetInt("Highscore" + levelNumber, 0);
+        
     }
 
 
     // Update is called once per frame
     void Update()
     {
-        
+        StarDisplayScore();
     }
 
     public void AddScore(int scoreAdd)
@@ -30,8 +35,39 @@ public class ScoreManager : MonoBehaviour
         score += scoreAdd;
         scoreText.text = "Score: " + score.ToString();
 
-        //highscore saving
+        //If score is greater than highscore, use score
         if (highScore < score)
-            PlayerPrefs.SetInt("Highscore", score);
+            PlayerPrefs.SetInt("Highscore" + levelNumber, score);
+    }
+
+   
+    public void StarDisplayScore()
+    {
+        switch (score)
+        {
+            case 0:
+                star1.SetActive(false);
+                star2.SetActive(false);
+                star3.SetActive(false);
+                break;
+            //If score is 10 (1 coin collected) show 1 star on levelselect
+            case 10:
+                star1.SetActive(true);
+                star2.SetActive(false);
+                star3.SetActive(false);
+                break;
+            //If score is 20 (2 coins collected) show 2 stars on levelSelect 
+            case 20:
+                star1.SetActive(true);
+                star2.SetActive(true);
+                star3.SetActive(false);
+                break;
+            //If score is 30 (3 coins collected) show 3 stars on levelSelect 
+            case 30:
+                star1.SetActive(true);
+                star2.SetActive(true);
+                star3.SetActive(true);
+                break;
+        }
     }
 }
