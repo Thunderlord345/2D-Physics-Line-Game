cd11933da35b2d2a81f4b736038c5e083ec094a7
Thunderlord345 <gregoryganis@gmail.com>
2023-01-01 04:03:28 +0800

    LineRopeToggle Script

--- Assets/Scripts/LineRopeToggle.cs
+++ Assets/Scripts/LineRopeToggle.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LineRopeToggle : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Button ropeCutter;
+    public Button lineDrawer;
+
+    public Image scissors;
+    public Image line;
+
+    Color scissorsAlpha;
+    Color lineAlpha;
+
+    private void Start()
     {
-        
+        //Assign color to image;
+        scissorsAlpha = scissors.color;
+        lineAlpha = line.color;
+
+    }
+    public void RopeCutter()
+    {
+        //Buttons enabled & disabled
+        ropeCutter.enabled = true;
+        lineDrawer.enabled = false;
+
+        //Alpha 
+        scissorsAlpha.a = 1f;
+        lineAlpha.a = 0.6f;
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void LineDrawer()
     {
-        
+        lineDrawer.enabled = true;
+        ropeCutter.enabled = false;
+
+        scissorsAlpha.a = 0.6f;
+        lineAlpha.a = 1f;
     }
 }
