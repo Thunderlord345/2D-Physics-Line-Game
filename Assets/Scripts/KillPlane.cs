e016a49a3bfc10bac2708876dc5498476ef173b9
Thunderlord345 <gregoryganis@gmail.com>
2023-01-05 20:57:56 +0800

    KillPlane trigger fix + bg change

--- Assets/Scripts/KillPlane.cs
+++ Assets/Scripts/KillPlane.cs
@@ -10,7 +10,7 @@ public class KillPlane : MonoBehaviour
     {
         loseScreen.SetActive(false);
     }
-    private void OnTriggerEnter2D(Collider other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.tag == "Ball")
         {
