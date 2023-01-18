43522a9ba67f71d0f4229ad89b6fce40856628ed
Thunderlord345 <gregoryganis@gmail.com>
2023-01-18 23:28:57 +0800

    Tutorial Instructions

---
+++ Assets/Scripts/TutorialDialogue/DialogueManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    public GameObject[] instructions;
+    LineFactory lf;
+    int instructIndex;
+
+    public GameObject levelAudio;
+    private void Start()
+    {
+        lf = FindObjectOfType<LineFactory>();
+
+        lf.isRunning = false;
+        levelAudio.SetActive(false);
+        
+    }
+
+    private void Update()
+    {
+        TutorialPopUp();
+    }
+
+    void TutorialPopUp()
+    {
+        for (int i = 0; i < instructions.Length; i++)
+        {
+            if(i == instructIndex)
+            {
+                instructions[i].SetActive(true);
+            }
+            else
+            {
+                instructions[i].SetActive(false);
+            }
+        }
+        // Move along the array with every tap
+        if (Input.GetMouseButtonDown(0))
+        {
+            instructIndex++;
+        }
+
+        if(instructIndex == instructions.Length)
+        {
+            lf.isRunning = true;
+            levelAudio.SetActive(true);
+        }
+    }
+}
